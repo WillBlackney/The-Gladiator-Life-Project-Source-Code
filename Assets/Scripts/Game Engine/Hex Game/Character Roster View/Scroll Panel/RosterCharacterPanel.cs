03ad92b0466e185b10d8fdfd9a80660aeb9f78b6
William Blackney <william.blackney@hotmail.com>
2022-03-07 22:20:31 +0100

    Started work on character roster scroll view

---
+++ Assets/Scripts/Game Engine/Hex Game/Character Roster View/Scroll Panel/RosterCharacterPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using CardGameEngine.UCM;
+namespace HexGameEngine.UI
+{
+    public class RosterCharacterPanel : MonoBehaviour
+    {
+        // Components
+        #region
+        [Header("Core Components")]
+        [SerializeField] TextMeshProUGUI nameText;
+        [SerializeField] UniversalCharacterModel portaitModel;
+
+        [Header("Health Bar Components")]
+        [SerializeField] Slider healthBar;
+        [SerializeField] TextMeshProUGUI  healthText;
+
+        [Header("Stress Bar Components")]
+        [SerializeField] Slider stressBar;
+        [SerializeField] TextMeshProUGUI stressText;
+        #endregion
+    }
+}
