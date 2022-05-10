947fa97d218782c005e25f02fcbdf98f4a570d3d
William Blackney <william.blackney@hotmail.com>
2022-05-10 22:18:22 +0200

    more play testing fixes + balance changes

--- Assets/Scripts/Game Engine/Hex Game/Combat/CombatRewardData.cs
+++ Assets/Scripts/Game Engine/Hex Game/Combat/CombatRewardData.cs
@@ -19,24 +19,30 @@ namespace HexGameEngine.Characters
                 BuildAsBasicReward(this);
             else if (difficulty == CombatDifficulty.Elite)
                 BuildAsEliteReward(this);
+            else if (difficulty == CombatDifficulty.Boss)
+                BuildAsBossReward(this);
             // to do: boss reward
         }
 
-        public void BuildAsBasicReward(CombatRewardData crd)
+        private void BuildAsBasicReward(CombatRewardData crd)
         {
             crd.goldAmount = RandomGenerator.NumberBetween(125, 150);
             crd.abilityAwarded = AbilityController.Instance.GetRandomAbilityTomeAbility();
             crd.item = ItemController.Instance.GenerateNewItemWithRandomEffects(ItemController.Instance.GetRandomLootableItemByRarity(Rarity.Rare));
 
         }
-        public void BuildAsEliteReward(CombatRewardData crd)
+        private void BuildAsEliteReward(CombatRewardData crd)
         {
             crd.goldAmount = RandomGenerator.NumberBetween(225, 250);
             crd.abilityAwarded = AbilityController.Instance.GetRandomAbilityTomeAbility();
             crd.item = ItemController.Instance.GenerateNewItemWithRandomEffects(ItemController.Instance.GetRandomLootableItemByRarity(Rarity.Epic));
         }
-        public void BuildAsBossReward()
+        private void BuildAsBossReward(CombatRewardData crd)
         {
+            // to do: update this when we create legendary items + define boss reward paramters
+            crd.goldAmount = RandomGenerator.NumberBetween(225, 250);
+            crd.abilityAwarded = AbilityController.Instance.GetRandomAbilityTomeAbility();
+            crd.item = ItemController.Instance.GenerateNewItemWithRandomEffects(ItemController.Instance.GetRandomLootableItemByRarity(Rarity.Epic));
 
         }
 
