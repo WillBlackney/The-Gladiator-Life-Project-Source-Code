0bfca3a18a87b14fb4e4cddd8f78ecd7bd8f1eb7
William Blackney <william.blackney@hotmail.com>
2022-06-14 14:49:10 +0200

    created initial camera zoom on feature building logic

--- Assets/Scripts/Game Engine/Hex Game/UI/TownBuildingView.cs
+++ Assets/Scripts/Game Engine/Hex Game/UI/TownBuildingView.cs
@@ -5,10 +5,11 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using DG.Tweening;
+using HexGameEngine.CameraSystems;
 
 namespace HexGameEngine.TownFeatures
 {
-    public class TownBuildingView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class TownBuildingView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
         [SerializeField] Color normalColor;
         [SerializeField] Color mouseOverColor;
@@ -19,13 +20,62 @@ namespace HexGameEngine.TownFeatures
         [Space(10)]
         [SerializeField] CanvasGroup popUpCg;       
         [SerializeField] CanvasGroup outlineCg;
+        [SerializeField] RectTransform cameraZoomToPoint;
+
+        private static bool blockMouseActions = false;
+        private static bool rightClicked = false;
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                rightClicked = true;
+            }
+            else rightClicked = false;
+        }
+
+
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (blockMouseActions) return;
+            blockMouseActions = true;
+
+            if (rightClicked)
+            {
+                Debug.Log("Move camera away");
+                var c = CameraController.Instance.MainCamera;
+                c.DOOrthoSize(5, 0.75f);
+                Sequence seq = DOTween.Sequence();               
+                seq.Append(c.transform.DOMove(new Vector3(0, 0, -15), 0.75f));                
+                seq.OnComplete(() => { blockMouseActions = false; });
+                return;
+            }
+
+            foreach (Image i in buildingImages)
+                i.color = normalColor;
+
+            outlineCg.DOKill();
+            popUpRect.DOKill();
+            popUpCg.DOKill();
+
+            outlineCg.DOFade(0, 0.1f);
+            popUpRect.DOMove(startPos.position, 0.25f);
+            popUpCg.DOFade(0f, 0.15f);
+
+            var cam = CameraController.Instance.MainCamera;
+            cam.DOOrthoSize(2.5f, 0.75f);
+            Sequence s = DOTween.Sequence();
+            s.Append(cam.transform.DOMove(new Vector3(cameraZoomToPoint.position.x, cameraZoomToPoint.position.y, -15), 0.75f));           
+            s.OnComplete(() => { blockMouseActions = false; });
+        }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            
+            //if (blockMouseActions) return; 
+
             foreach(Image i in buildingImages)            
                 i.color = mouseOverColor;
-
             
             outlineCg.DOKill();
             popUpRect.DOKill();
@@ -39,6 +89,8 @@ namespace HexGameEngine.TownFeatures
 
         public void OnPointerExit(PointerEventData eventData)
         {
+           // if (blockMouseActions) return;
+
             foreach (Image i in buildingImages)
                 i.color = normalColor;
 
